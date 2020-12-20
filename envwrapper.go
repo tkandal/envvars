8d34ee7081317d38494b6d4fbe0996444bae18b9
Trond Kandal <Trond.Kandal@ntnu.no>
2020-12-20 14:08:14 +0100

    Formatting,

--- envwrapper.go
+++ envwrapper.go
@@ -7,8 +7,9 @@ import (
 	"strings"
 )
 
-// EnvWrapper checks if environment variables are prefixed with the program's name
-// plus separator, and translates them to environment variables without the prefix.
+// EnvWrapper checks if environment variables are prefixed with the
+// program's name  plus a separator, and translates them to environment
+// variables without the prefix.
 func EnvWrapper(sep string, envs ...string) error {
 	prefix := strings.ToUpper(filepath.Base(os.Args[0])) + sep
 	for _, env := range envs {
